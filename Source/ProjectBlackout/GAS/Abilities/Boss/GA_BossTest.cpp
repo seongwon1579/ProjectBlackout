1b6f40a9ab6633ac7b5c215bfc043a5fcadd0f76
DESKTOP-BBED2Q3\KGA <seongwon1579@gmail.com>
2026-04-29 14:27:04 +0900

    Feat: 적이 가하는 데미지 콜리전 구현

--- Source/ProjectBlackout/GAS/Abilities/Boss/GA_BossTest.cpp
+++ Source/ProjectBlackout/GAS/Abilities/Boss/GA_BossTest.cpp
@@ -1,16 +1,24 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "GAS/Abilities/Boss/GA_BossTest.h"
+
+#include "AbilitySystemComponent.h"
 #include "Abilities/Tasks/AbilityTask_PlayMontageAndWait.h"
+#include "Abilities/Tasks/AbilityTask_WaitGameplayEvent.h"
 #include "Characters/BlackoutBossCharacter.h"
+#include "GAS/Tasks/AbilityTask_BossMeleeSweep.h"
+#include "GameplayTags/BlackoutGameplayTags.h"
+#include "Interfaces/BlackoutDamageable.h"
 #include "MotionWarpingComponent.h"
 
 UGA_BossTest::UGA_BossTest()
 {
-	AbilityTags.AddTag(FGameplayTag::RequestGameplayTag(TEXT("Character.Enemy.Attack1")));
+	FGameplayTagContainer Tags;
+	Tags.AddTag(BlackoutGameplayTags::Character_Enemy_Attack1);
+	SetAssetTags(Tags);
 }
 
+//TODO: 모든 기능을 한번에 넣었음 나중에 분리 필요
 void UGA_BossTest::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo,
                                    const FGameplayAbilityActivationInfo ActivationInfo,
                                    const FGameplayEventData* TriggerEventData)
@@ -19,15 +27,14 @@ void UGA_BossTest::ActivateAbility(const FGameplayAbilitySpecHandle Handle, cons
 
 	UE_LOG(LogTemp, Warning, TEXT("ActivateAbility"));
 
-	// WarpTarget 설정: 몽타주 재생 전에 반드시 먼저 설정
 	ABlackoutBossCharacter* Boss = Cast<ABlackoutBossCharacter>(ActorInfo->AvatarActor.Get());
 
 	if (Boss && Boss->MotionWarpingComponent)
 	{
+		// TODO : ActorInfo에 플레이어를 사용 하는것으로 변경 필요
 		APawn* Target = Boss->GetWorld()->GetFirstPlayerController()->GetPawn();
 		if (Target)
 		{
-			// bFollowComponent=true: 매 프레임 타겟 위치를 추적 — 플레이어가 움직여도 따라감
 			Boss->MotionWarpingComponent->AddOrUpdateWarpTargetFromComponent(
 				WarpTargetName,
 				Target->GetRootComponent(),
@@ -38,7 +45,7 @@ void UGA_BossTest::ActivateAbility(const FGameplayAbilitySpecHandle Handle, cons
 		}
 	}
 
-	UAbilityTask_PlayMontageAndWait* Task = UAbilityTask_PlayMontageAndWait::CreatePlayMontageAndWaitProxy(
+	UAbilityTask_PlayMontageAndWait* MontageTask = UAbilityTask_PlayMontageAndWait::CreatePlayMontageAndWaitProxy(
 		this,
 		NAME_None,
 		Montage,
@@ -46,17 +53,24 @@ void UGA_BossTest::ActivateAbility(const FGameplayAbilitySpecHandle Handle, cons
 		NAME_None,
 		true,
 		1.f
-		);
-	
-	Task->OnCompleted.AddDynamic(this, &UGA_BossTest::OnMontageEnded);
-	
-	Task->ReadyForActivation();
+	);
+	MontageTask->OnCompleted.AddDynamic(this, &UGA_BossTest::OnMontageEnded);
+	MontageTask->ReadyForActivation();
+
+	// ANS Begin → sweep 시작
+	UAbilityTask_WaitGameplayEvent* WaitStartTask = UAbilityTask_WaitGameplayEvent::WaitGameplayEvent(
+		this,
+		BlackoutGameplayTags::Event_Enemy_Attack_SweepStart,
+		nullptr,
+		true
+	);
+	WaitStartTask->EventReceived.AddDynamic(this, &UGA_BossTest::OnSweepStartEvent);
+	WaitStartTask->ReadyForActivation();
 }
 
 void UGA_BossTest::EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo,
 	const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled)
 {
-	// WarpTarget 정리
 	ABlackoutBossCharacter* Boss = Cast<ABlackoutBossCharacter>(ActorInfo->AvatarActor.Get());
 	if (Boss && Boss->MotionWarpingComponent)
 	{
@@ -77,4 +91,60 @@ void UGA_BossTest::OnMontageInterrupted()
 
 void UGA_BossTest::OnMontageCancelled()
 {
+}
+
+void UGA_BossTest::OnSweepStartEvent(FGameplayEventData Payload)
+{
+	ActiveSweepTask = UAbilityTask_BossMeleeSweep::CreateSweepTask(this, SweepStartSocket, SweepEndSocket, SweepRadius);
+	ActiveSweepTask->OnHit.AddDynamic(this, &UGA_BossTest::OnMeleeSweepHit);
+	ActiveSweepTask->ReadyForActivation();
+
+	// ANS End → sweep 종료
+	UAbilityTask_WaitGameplayEvent* WaitEndTask = UAbilityTask_WaitGameplayEvent::WaitGameplayEvent(
+		this,
+		BlackoutGameplayTags::Event_Enemy_Attack_SweepEnd,
+		nullptr,
+		true
+	);
+	WaitEndTask->EventReceived.AddDynamic(this, &UGA_BossTest::OnSweepEndEvent);
+	WaitEndTask->ReadyForActivation();
+}
+
+void UGA_BossTest::OnSweepEndEvent(FGameplayEventData Payload)
+{
+	if (ActiveSweepTask)
+	{
+		ActiveSweepTask->EndTask();
+		ActiveSweepTask = nullptr;
+	}
+}
+
+void UGA_BossTest::OnMeleeSweepHit(const FHitResult& HitResult)
+{
+	if (!DamageEffectClass)
+	{
+		return;
+	}
+
+	AActor* HitActor = HitResult.GetActor();
+	IBlackoutDamageable* Damageable = Cast<IBlackoutDamageable>(HitActor);
+	if (!Damageable)
+	{
+		return;
+	}
+
+	UAbilitySystemComponent* OwnerASC = GetAbilitySystemComponentFromActorInfo();
+	if (!OwnerASC)
+	{
+		return;
+	}
+
+	FGameplayEffectContextHandle EffectContext = OwnerASC->MakeEffectContext();
+	EffectContext.AddSourceObject(GetAvatarActorFromActorInfo());
+
+	FGameplayEffectSpecHandle SpecHandle = OwnerASC->MakeOutgoingSpec(DamageEffectClass, GetAbilityLevel(), EffectContext);
+	if (SpecHandle.IsValid())
+	{
+		Damageable->ReceiveDamageFromHitbox(SpecHandle, HitResult.BoneName);
+	}
 }
--- Source/ProjectBlackout/GAS/Abilities/Boss/GA_BossTest.h
+++ Source/ProjectBlackout/GAS/Abilities/Boss/GA_BossTest.h
@@ -4,42 +4,74 @@
 
 #include "CoreMinimal.h"
 #include "GAS/Abilities/BlackoutBossGameplayAbility.h"
+#include "GameplayEffect.h"
 #include "GA_BossTest.generated.h"
 
+struct FHitResult;
+struct FGameplayEventData;
+class UAbilityTask_BossMeleeSweep;
+
 /**
- * 
+ *
  */
 UCLASS()
 class PROJECTBLACKOUT_API UGA_BossTest : public UBlackoutBossGameplayAbility
 {
 	GENERATED_BODY()
-	
-	
+
 public:
 	UGA_BossTest();
+
 	UPROPERTY(EditDefaultsOnly, Category = "Animation")
 	TObjectPtr<UAnimMontage> Montage;
+
+	/** 히트 시 적용할 데미지 GE */
+	UPROPERTY(EditDefaultsOnly, Category = "Combat")
+	TSubclassOf<UGameplayEffect> DamageEffectClass;
+
+	/** 스윕 시작 소켓 */
+	UPROPERTY(EditDefaultsOnly, Category = "Combat")
+	FName SweepStartSocket = TEXT("Socket_Jaw_Root");
+
+	/** 스윕 끝 소켓 */
+	UPROPERTY(EditDefaultsOnly, Category = "Combat")
+	FName SweepEndSocket = TEXT("Socket_Jaw_Tip");
+
+	/** 스윕 구체 반지름 (cm) */
+	UPROPERTY(EditDefaultsOnly, Category = "Combat")
+	float SweepRadius = 30.f;
 
 protected:
 	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
 	virtual void EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled) override;
-	
+
 	UFUNCTION()
 	void OnMontageEnded();
-	
+
 	UFUNCTION()
 	void OnMontageInterrupted();
-	
+
 	UFUNCTION()
 	void OnMontageCancelled();
-	
+
+	UFUNCTION()
+	void OnSweepStartEvent(FGameplayEventData Payload);
+
+	UFUNCTION()
+	void OnSweepEndEvent(FGameplayEventData Payload);
+
+	UFUNCTION()
+	void OnMeleeSweepHit(const FHitResult& HitResult);
+
 	UPROPERTY(EditDefaultsOnly, Category = "MotionWarping")
 	FName WarpTargetName = TEXT("MW_Target");
-	
+
 	UPROPERTY(EditDefaultsOnly, Category = "MotionWarping")
 	float WarpReferenceDistance = 400.f;
-	
+
 	UPROPERTY(EditDefaultsOnly, Category = "MotionWarping")
 	float WarpMinPlayRate = 0.3f;
-	
+
+private:
+	TObjectPtr<UAbilityTask_BossMeleeSweep> ActiveSweepTask;
 };
