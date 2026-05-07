737ed0b93f08a80465113ef44df3627b1ff66e7d
DESKTOP-BBED2Q3\KGA <seongwon1579@gmail.com>
2026-05-07 16:25:09 +0900

    Refactor: 변수명 변경 및 불필요한 헬퍼클래스 이용 제거

--- Source/ProjectBlackout/AI/BehaviorTree/Services/BTService_UpdateTargetData.cpp
+++ Source/ProjectBlackout/AI/BehaviorTree/Services/BTService_UpdateTargetData.cpp
@@ -12,34 +12,43 @@ UBTService_UpdateTargetData::UBTService_UpdateTargetData()
 	NodeName = "Update Target Data";
 	Interval = 0.1f;
 	RandomDeviation = 0.05f;
+
+	bCreateNodeInstance = true;
 }
 
 void UBTService_UpdateTargetData::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
 {
 	Super::TickNode(OwnerComp, NodeMemory, DeltaSeconds);
-	
-	UBlackboardComponent* BB = UBTNodeHelper::GetBlackboard(OwnerComp);
+	UpdateBlackboard(OwnerComp);
+}
+
+void UBTService_UpdateTargetData::UpdateBlackboard(UBehaviorTreeComponent& OwnerComp)
+{
+	if (!OwnerComp.IsRunning()) return;
+
+	UBlackboardComponent* BB = OwnerComp.GetBlackboardComponent();
 	if (!BB) return;
-	
+
 	// AggroComponent에서 최고 위협 타겟 가져오기
-	ABlackoutBossCharacter* Enemy = UBTNodeHelper::GetAIPawn<ABlackoutBossCharacter>(OwnerComp);
-	if (!Enemy) return;
-	
+	ABlackoutBossCharacter* Owner = UBTNodeHelper::GetAIPawn<ABlackoutBossCharacter>(OwnerComp);
+	if (!Owner) return;
+
 	// APawn* CurrentTarget = Enemy && Enemy->AggroComponent
 	// 	? Enemy->AggroComponent->GetHighestAggroTarget()
 	// 	: nullptr;
-	
-	APawn* CurrentTarget  = GetWorld()->GetFirstPlayerController()->GetPawn();
-	
-	if (!CurrentTarget) return;
-	
-	const FVector AILocation     = Enemy->GetActorLocation();
-	const FVector TargetLocation = CurrentTarget->GetActorLocation();
-	const FVector ToTarget       = TargetLocation - AILocation;
 
-	BB->SetValueAsObject(CurrentTargetKey.SelectedKeyName, CurrentTarget);
-	BB->SetValueAsVector(CurrentTargetLocationKey.SelectedKeyName,     TargetLocation);
-	BB->SetValueAsFloat(DistanceBTWActorsKey.SelectedKeyName,      ToTarget.Size2D());
+	APlayerController* PC = GetWorld()->GetFirstPlayerController();
+	APawn* Target = PC ? PC->GetPawn() : nullptr;
+
+	if (!Target) return;
+
+	const FVector OwnerLocation = Owner->GetActorLocation();
+	const FVector TargetLocation = Target->GetActorLocation();
+	const FVector ToTarget = TargetLocation - OwnerLocation;
+
+	BB->SetValueAsObject(TargetKey.SelectedKeyName, Target);
+	BB->SetValueAsVector(TargetLocationKey.SelectedKeyName, TargetLocation);
+	BB->SetValueAsFloat(DistanceBTWActorsKey.SelectedKeyName, ToTarget.Size2D());
 	BB->SetValueAsFloat(AngleBTWActorsKey.SelectedKeyName,
-		UKismetAnimationLibrary::CalculateDirection(ToTarget, Enemy->GetActorRotation()));
+	                    UKismetAnimationLibrary::CalculateDirection(ToTarget, Owner->GetActorRotation()));
 }
