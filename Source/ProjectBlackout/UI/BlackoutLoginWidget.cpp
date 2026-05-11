ebe753032aacb82d13ed39bedea7e2cec1ace98c
audidas <audidas@naver.com>
2026-05-11 14:22:49 +0900

    Feat: UMG_Login 모달 위젯 C++ 베이스 추가

---
+++ Source/ProjectBlackout/UI/BlackoutLoginWidget.cpp
@@ -0,0 +1,125 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BlackoutLoginWidget.h"
+
+#include "Framework/BlackoutMatchmakingSubsystem.h"
+
+#include "Components/Button.h"
+#include "Components/EditableTextBox.h"
+#include "Components/TextBlock.h"
+
+void UBlackoutLoginWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+	
+	if (LoginButton)
+	{
+		LoginButton->OnClicked.AddDynamic(this, &UBlackoutLoginWidget::HandleLoginClicked);
+	}
+	
+	if (CancelButton)
+	{
+		CancelButton->OnClicked.AddDynamic(this, &UBlackoutLoginWidget::HandleCancelClicked);
+	}
+	if (UGameInstance* GameInstance = GetGameInstance())
+	{
+		if (UBlackoutMatchmakingSubsystem* MatchmakingSubsystem = GameInstance->GetSubsystem<UBlackoutMatchmakingSubsystem>())
+		{
+			MatchmakingSubsystem->OnLoginComplete.AddDynamic(this, &UBlackoutLoginWidget::HandleLoginComplete);
+		}
+	}
+	
+	if (ErrorMessageText)
+	{
+		ErrorMessageText->SetText(FText::GetEmpty());
+	}
+}
+
+void UBlackoutLoginWidget::NativeDestruct()
+{
+	if (UGameInstance* GameInstance = GetGameInstance())
+	{
+		if (UBlackoutMatchmakingSubsystem* MatchmakingSubsystem = GameInstance->GetSubsystem<UBlackoutMatchmakingSubsystem>())
+		{
+			MatchmakingSubsystem->OnLoginComplete.RemoveDynamic(this, &UBlackoutLoginWidget::HandleLoginComplete);
+		}
+	}
+	Super::NativeDestruct();
+}
+
+void UBlackoutLoginWidget::HandleLoginClicked()
+{
+	if (!PlayerNameInput || !PasswordInput)
+	{
+		return;
+	}
+	
+	const FString PlayerName = PlayerNameInput->GetText().ToString().TrimStartAndEnd();
+	const FString Password = PasswordInput->GetText().ToString();
+	
+	if (PlayerName.IsEmpty()|| Password.IsEmpty())
+	{
+		if (ErrorMessageText)
+		{
+			ErrorMessageText->SetText(NSLOCTEXT("BlackoutLogin", "EmptyField","이름 / 비밀번호를 입력하세요"));
+		}
+		return;
+	}
+	
+	UGameInstance* GameInstance = GetGameInstance();
+	UBlackoutMatchmakingSubsystem* MatchmakingSubsystem = GameInstance ? GameInstance->GetSubsystem<UBlackoutMatchmakingSubsystem>() : nullptr;
+	if (!MatchmakingSubsystem)
+	{
+		return;
+	}
+	
+	SetBusy(true);
+	MatchmakingSubsystem->Login(PlayerName, Password);
+}
+
+void UBlackoutLoginWidget::HandleCancelClicked()
+{
+	OnLoginAttemptFinished.Broadcast(false, FString());
+	RemoveFromParent();
+}
+
+void UBlackoutLoginWidget::HandleLoginComplete(bool bSuccess)
+{
+	if (bSuccess)
+	{
+		FString PlayerName;
+		if (UGameInstance* GameInstance = GetGameInstance())
+		{
+			if (UBlackoutMatchmakingSubsystem* MatchmakingSubsystem = GameInstance->GetSubsystem<UBlackoutMatchmakingSubsystem>())
+			{
+				PlayerName = MatchmakingSubsystem->GetPlayerName();
+			}
+		}
+		OnLoginAttemptFinished.Broadcast(bSuccess, PlayerName);
+		RemoveFromParent();
+		return;
+	}
+	
+	if (ErrorMessageText)
+	{
+		ErrorMessageText->SetText(NSLOCTEXT("BlackoutLogin","LoginFailed","로그인 실패 다시 시도하세요"));
+	}
+	SetBusy(false);
+}
+
+void UBlackoutLoginWidget::SetBusy(bool bBusy)
+{
+	if (LoginButton)
+	{
+		LoginButton->SetIsEnabled(!bBusy);
+	}
+	if (CancelButton)
+	{
+		CancelButton->SetIsEnabled(!bBusy);
+	}
+	if (bBusy && ErrorMessageText)
+	{
+		ErrorMessageText->SetText(FText::GetEmpty());
+	}
+}
---
+++ Source/ProjectBlackout/UI/BlackoutLoginWidget.h
@@ -0,0 +1,61 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "BlackoutLoginWidget.generated.h"
+
+class UEditableTextBox;
+class UButton;
+class UTextBlock;
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnBlackoutLoginAttemptFinished, bool,bSuccess,const FString&, PlayerName);
+
+/**
+ *  로그인 모달 위젯,
+ *  UBlackoutMatchmakingSubsystem-> Login 트리거 , OnLoginComplete 결과
+ *  메인 메뉴는 OnLoginAttemptFinished 구독해서 상태 갱신
+ */
+UCLASS()
+class PROJECTBLACKOUT_API UBlackoutLoginWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+public:
+	
+	UPROPERTY(BlueprintAssignable, Category="Blackout|Login")
+	FOnBlackoutLoginAttemptFinished  OnLoginAttemptFinished;
+	
+protected:
+	virtual void NativeConstruct() override;
+	virtual void NativeDestruct() override;
+	
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UEditableTextBox> PlayerNameInput;
+	
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UEditableTextBox> PasswordInput;
+	
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UButton> LoginButton;
+	
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UButton> CancelButton;
+	
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UTextBlock> ErrorMessageText;
+	
+private:
+	UFUNCTION()
+	void HandleLoginClicked();
+	
+	UFUNCTION()
+	void HandleCancelClicked();
+	
+	UFUNCTION()
+	void HandleLoginComplete(bool bSuccess);
+	
+	// 요청 중 LoginButton/Cancel 비활성 , ErrorMessage 클리어
+	void SetBusy(bool bBusy);
+};
