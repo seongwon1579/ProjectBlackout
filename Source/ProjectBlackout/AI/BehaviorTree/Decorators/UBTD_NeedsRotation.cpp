20156ec69ca44420c01514a5b565d4ae0a18c56c
DESKTOP-BBED2Q3\KGA <seongwon1579@gmail.com>
2026-05-12 18:00:12 +0900

    Feat: 현재 각도가 몇인지 에디터에서 확인 가능하도록 변경

--- Source/ProjectBlackout/AI/BehaviorTree/Decorators/UBTD_NeedsRotation.cpp
+++ Source/ProjectBlackout/AI/BehaviorTree/Decorators/UBTD_NeedsRotation.cpp
@@ -14,8 +14,19 @@ bool UUBTD_NeedsRotation::CalculateRawConditionValue(UBehaviorTreeComponent& Own
 {
 	auto BB = OwnerComp.GetBlackboardComponent();
 	if (!BB) return false;
-	
+
 	float SignedAngle = BB->GetValueAsFloat(SignedAngleKey.SelectedKeyName);
+
+	if (GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(
+			static_cast<uint64>(GetUniqueID()),
+			0.0f,
+			FMath::Abs(SignedAngle) >= AngleThreshold ? FColor::Red : FColor::Green,
+			FString::Printf(TEXT("[NeedsRotation] Angle: %.1f / Threshold: %.1f"), SignedAngle, AngleThreshold)
+		);
+	}
+
 	return FMath::Abs(SignedAngle) >= AngleThreshold;
 }
 
