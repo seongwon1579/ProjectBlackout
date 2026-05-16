3069546e7513fa9b6e0157fe69fd810ba21ae7a4
우니\seong <seongwon1579@gmail.com>
2026-05-16 14:31:04 +0900

    Feat: Projectile 생성 시점 노티파이 추가

---
+++ Source/ProjectBlackout/Animation/Notifies/Enemy/BOAnimNotify_SpawnProjectile.h
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "Animation/AnimNotifies/AnimNotify.h"
+#include "BOAnimNotify_SpawnProjectile.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTBLACKOUT_API UBOAnimNotify_SpawnProjectile : public UAnimNotify
+{
+	GENERATED_BODY()
+	
+public:
+	virtual void Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference) override;
+	virtual FString GetNotifyName_Implementation() const override;
+	
+	UPROPERTY(EditAnywhere, Category = "Event")
+	FGameplayTag EventTag;
+};
