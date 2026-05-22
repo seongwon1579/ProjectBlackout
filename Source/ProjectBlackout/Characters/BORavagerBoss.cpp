b12e7ad8d6e6d8883a333ddc014da71188de2523
DESKTOP-BBED2Q3\KGA <seongwon1579@gmail.com>
2026-05-22 18:04:01 +0900

    Fix: 이후에 ravager를 위한 클래스로 사용가능 현재는 로직 제거

--- Source/ProjectBlackout/Characters/BORavagerBoss.cpp
+++ Source/ProjectBlackout/Characters/BORavagerBoss.cpp
@@ -1,18 +1,59 @@
 #include "Characters/BORavagerBoss.h"
 
-
-ABORavagerBoss::ABORavagerBoss()
-{
-	PrimaryActorTick.bCanEverTick = true;
-	AnimPlayRateMultiplier = 1.0f;
-	SummonedMinionCount = 0;
-}
+#include "AbilitySystemComponent.h"
+#include "BlackoutAbilitySystemComponent.h"
+#include "BlackoutBaseAttributeSet.h"
+#include "Abilities/GameplayAbility.h"
 
 
-void ABORavagerBoss::BeginPlay()
-{
-	Super::BeginPlay();
-	
-	if (!HasAuthority()) return;
-	
-}
+// ABORavagerBoss::ABORavagerBoss()
+// {
+// 	PrimaryActorTick.bCanEverTick = true;
+// }
+//
+// UDataAsset* ABORavagerBoss::GetPatternData(FGameplayTag AbilityTag) const
+// {
+// 	const TObjectPtr<UBORavagerPatternData>* Found = BossPatternData.Find(AbilityTag);
+// 	return Found ? Found->Get() : nullptr;
+// }
+//
+// void ABORavagerBoss::SetData()
+// {
+// 	UAbilitySystemComponent* ASC = GetAbilitySystemComponent();
+// 	if (!ASC) return;
+// 	
+// 	for (const auto& [Tag, Data] : BossPatternData)
+// 	{
+// 		if (!Data || !Data->GrantedAbility) continue;
+// 		ASC->GiveAbility(FGameplayAbilitySpec(Data->GrantedAbility, 1));
+// 	}
+// 	
+// 	if (AbilitySystemComponent)
+// 	{
+// 		AbilitySystemComponent->InitAbilityActorInfo(this, this);
+//
+// 		if (BaseAttributeSet && BossData)
+// 		{
+// 			AbilitySystemComponent->SetNumericAttributeBase(
+// 				UBlackoutBaseAttributeSet::GetMaxHealthAttribute(),
+// 				BossData->MaxHealth);
+// 			AbilitySystemComponent->SetNumericAttributeBase(
+// 				UBlackoutBaseAttributeSet::GetHealthAttribute(),
+// 				BossData->MaxHealth);
+// 		}
+// 	}
+// }
+//
+// EBOBossPhase ABORavagerBoss::DetermineTargetPhase(float HealthRatio) const
+// {
+// 	return HealthRatio <= 0.5f ? EBOBossPhase::Phase2 : EBOBossPhase::Phase1;
+// }
+//
+// FText ABORavagerBoss::GetBossDisplayName() const
+// {
+// 	if (BossData->IsValid())
+// 	{
+// 		return BossData->Name;
+// 	}
+// 	return FText::FromString(TEXT("Ravager"));
+// }
