8e0deb43f9efddf5fc0bc847d59f7c70d0f78412
DESKTOP-BBED2Q3\KGA <seongwon1579@gmail.com>
2026-05-22 11:45:01 +0900

    Refactor: 공격가능 범위까지 추적 가능한지를 계산하는 로직을 각 공격마다 데이터어셋에서 받아와 최신화 하도록 변경

--- Source/ProjectBlackout/AI/BehaviorTree/Services/BTS_CheckChaseDistance.h
+++ Source/ProjectBlackout/AI/BehaviorTree/Services/BTS_CheckChaseDistance.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "BORavagerData.h"
 #include "BehaviorTree/BTService.h"
 #include "BTS_CheckChaseDistance.generated.h"
 
@@ -19,16 +20,29 @@ public:
 
 	virtual void TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) override;
 	virtual void InitializeFromAsset(UBehaviorTree& Asset) override;
+	virtual void OnBecomeRelevant(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
 	virtual void OnCeaseRelevant(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
 	
 	UPROPERTY(EditAnywhere, Category = "Blackout|Blacboard|Input")
 	FBlackboardKeySelector DistanceBTWActors;
 	
+	UPROPERTY(EditAnywhere, Category = "Blackout|Blacboard|Input")
+	FBlackboardKeySelector Target;
+	
+	UPROPERTY(EditAnywhere, Category = "Blackout|Blacboard|Input")
+	FBlackboardKeySelector ActiveAbilityTagKey;
+	
 	UPROPERTY(EditAnywhere, Category = "Blackout|Blacboard|Output")
 	FBlackboardKeySelector bOnTrigger;
+	
+	UPROPERTY(EditAnywhere, Category = "Blackboard|Blackboard|Ouput")
+	FBlackboardKeySelector AcceptanceRadiusKey;
 
 protected:
 
-	UPROPERTY(EditAnywhere, Category = "Blackout|Parmas")
-	float Threshold;
+	// UPROPERTY(EditAnywhere, Category = "Blackout|Parmas")
+	// float Threshold;
+	
+	UPROPERTY(Transient)
+	TObjectPtr<UBORavagerData> CachedData;
 };
