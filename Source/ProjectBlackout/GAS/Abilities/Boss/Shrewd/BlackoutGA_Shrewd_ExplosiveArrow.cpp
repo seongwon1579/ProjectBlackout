eee99af6a1b1a55dccd7a0c7b3bf524640142904
DESKTOP-BBED2Q3\KGA <seongwon1579@gmail.com>
2026-05-29 16:49:24 +0900

    Feat: 추후 개발

--- Source/ProjectBlackout/GAS/Abilities/Boss/Shrewd/BlackoutGA_Shrewd_ExplosiveArrow.cpp
+++ Source/ProjectBlackout/GAS/Abilities/Boss/Shrewd/BlackoutGA_Shrewd_ExplosiveArrow.cpp
@@ -1,13 +1,2 @@
-#include "GAS/Abilities/Boss/Shrewd/GA_Shrewd_ExplosiveArrow.h"
+#include "GAS/Abilities/Boss/Shrewd/BlackoutGA_Shrewd_ExplosiveArrow.h"
 
-UGA_Shrewd_ExplosiveArrow::UGA_Shrewd_ExplosiveArrow()
-{
-}
-
-void UGA_Shrewd_ExplosiveArrow::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
-{
-	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
-	
-	// TODO: 투사체 스폰 및 폭발 시 SplashRadius 기반 광역 피해
-	EndAbility(Handle, ActorInfo, ActivationInfo, true, false);
-}
--- Source/ProjectBlackout/GAS/Abilities/Boss/Shrewd/BlackoutGA_Shrewd_ExplosiveArrow.h
+++ Source/ProjectBlackout/GAS/Abilities/Boss/Shrewd/BlackoutGA_Shrewd_ExplosiveArrow.h
@@ -2,23 +2,11 @@
 
 #include "CoreMinimal.h"
 #include "BlackoutGA_Shrewd_Base.h"
-#include "GAS/Abilities/Boss/Ravager/BlackoutGA_Ravager_Base.h"
-#include "GA_Shrewd_ExplosiveArrow.generated.h"
+#include "BlackoutGA_Shrewd_ExplosiveArrow.generated.h"
 
-/**
- * 발판 페이즈 — 폭발 속성 단발 투사체
- */
 UCLASS()
-class PROJECTBLACKOUT_API UGA_Shrewd_ExplosiveArrow : public UBlackoutGA_Shrewd_Base
+class PROJECTBLACKOUT_API UBlackoutGA_Shrewd_ExplosiveArrow : public UBlackoutGA_Shrewd_Base
 {
 	GENERATED_BODY()
-
-public:
-	UGA_Shrewd_ExplosiveArrow();
-
-protected:
-	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
-
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Blackout|Ability")
-	float SplashRadius = 500.0f;
+	
 };
