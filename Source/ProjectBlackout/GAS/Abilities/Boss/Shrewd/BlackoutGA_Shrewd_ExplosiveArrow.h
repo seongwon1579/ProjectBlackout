4290be440a2796cdc649f947522b4282f97bb0cf
DESKTOP-BBED2Q3\KGA <seongwon1579@gmail.com>
2026-05-29 16:39:44 +0900

    Fix: 클래스 정리

--- Source/ProjectBlackout/GAS/Abilities/Boss/Shrewd/BlackoutGA_Shrewd_ExplosiveArrow.h
+++ Source/ProjectBlackout/GAS/Abilities/Boss/Shrewd/BlackoutGA_Shrewd_ExplosiveArrow.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "BlackoutGA_Shrewd_Base.h"
 #include "GAS/Abilities/Boss/Ravager/BlackoutGA_Ravager_Base.h"
 #include "GA_Shrewd_ExplosiveArrow.generated.h"
 
@@ -8,7 +9,7 @@
  * 발판 페이즈 — 폭발 속성 단발 투사체
  */
 UCLASS()
-class PROJECTBLACKOUT_API UGA_Shrewd_ExplosiveArrow : public UBlackoutGA_Ravager_Base
+class PROJECTBLACKOUT_API UGA_Shrewd_ExplosiveArrow : public UBlackoutGA_Shrewd_Base
 {
 	GENERATED_BODY()
 
