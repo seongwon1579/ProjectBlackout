a902548d65e6e8fcd3ed595c73c90072c86f0f98
audidas <audidas@naver.com>
2026-05-29 10:10:07 +0900

    Chore: Wraith 텔레포트 코드 포맷 정리

--- Source/ProjectBlackout/GAS/Abilities/Minion/GA_Wraith_Teleport.cpp
+++ Source/ProjectBlackout/GAS/Abilities/Minion/GA_Wraith_Teleport.cpp
@@ -1,6 +1,5 @@
 ﻿// Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "GA_Wraith_Teleport.h"
 #include "GameplayTags/BlackoutGameplayTags.h"
 #include "Abilities/Tasks/AbilityTask_WaitGameplayEvent.h"
@@ -9,7 +8,6 @@
 #include "Components/PrimitiveComponent.h"
 #include "Components/WidgetComponent.h"
 #include "EnvironmentQuery/EnvQueryManager.h"
-
 
 UGA_Wraith_Teleport::UGA_Wraith_Teleport()
 {
@@ -22,14 +20,14 @@ UGA_Wraith_Teleport::UGA_Wraith_Teleport()
 
 void UGA_Wraith_Teleport::ActivateAbility(
 	const FGameplayAbilitySpecHandle Handle,
-	const FGameplayAbilityActorInfo* ActorInfo,
+	const FGameplayAbilityActorInfo *ActorInfo,
 	const FGameplayAbilityActivationInfo ActivationInfo,
-	const FGameplayEventData* TriggerEventData)
+	const FGameplayEventData *TriggerEventData)
 {
 	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
 
-	UAbilitySystemComponent* ASC = GetAbilitySystemComponentFromActorInfo();
-	AActor* Avatar = GetAvatarActorFromActorInfo();
+	UAbilitySystemComponent *ASC = GetAbilitySystemComponentFromActorInfo();
+	AActor *Avatar = GetAvatarActorFromActorInfo();
 	if (!CommitAbility(Handle, ActorInfo, ActivationInfo) || !ASC || !Avatar ||
 		!TeleportQuery || !TeleportAnimMontage)
 	{
@@ -50,18 +48,17 @@ void UGA_Wraith_Teleport::ActivateAbility(
 	// EQS 비동기 호출
 	FEnvQueryRequest Request(TeleportQuery, Avatar);
 	Request.Execute(EEnvQueryRunMode::SingleResult, this,
-	                &UGA_Wraith_Teleport::OnEQSFinished);
+					&UGA_Wraith_Teleport::OnEQSFinished);
 }
 
 void UGA_Wraith_Teleport::OnEQSFinished(TSharedPtr<FEnvQueryResult> Result)
 {
-	if (!Result.IsValid() || !Result->IsSuccessful() || Result->Items.Num() ==
-		0)
+	if (!Result.IsValid() || !Result->IsSuccessful() || Result->Items.Num() == 0)
 	{
 		// 텔레포트 취소
 		RemoveInvulnerableTag();
 		EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo,
-		           true, false);
+				   true, false);
 		return;
 	}
 
@@ -72,55 +69,53 @@ void UGA_Wraith_Teleport::OnEQSFinished(TSharedPtr<FEnvQueryResult> Result)
 void UGA_Wraith_Teleport::StartMontageAndWaitEvents()
 {
 	// Vanish 이벤트 대기
-	UAbilityTask_WaitGameplayEvent* VanishTask =
+	UAbilityTask_WaitGameplayEvent *VanishTask =
 		UAbilityTask_WaitGameplayEvent::WaitGameplayEvent(
 			this, BlackoutGameplayTags::Event_Wraith_Teleport_Vanish,
-			nullptr, true, true
-		);
-	
+			nullptr, true, true);
+
 	if (VanishTask)
 	{
 		VanishTask->EventReceived.AddDynamic(this, &UGA_Wraith_Teleport::OnVanishEvent);
 		VanishTask->ReadyForActivation();
 	}
-	
+
 	// Appear 이벤트 대기
-	UAbilityTask_WaitGameplayEvent* AppearTask = UAbilityTask_WaitGameplayEvent::WaitGameplayEvent(
-	this , BlackoutGameplayTags::Event_Wraith_Teleport_Appear, nullptr, true, true	
-	);
+	UAbilityTask_WaitGameplayEvent *AppearTask = UAbilityTask_WaitGameplayEvent::WaitGameplayEvent(
+		this, BlackoutGameplayTags::Event_Wraith_Teleport_Appear, nullptr, true, true);
 	if (AppearTask)
 	{
 		AppearTask->EventReceived.AddDynamic(this, &UGA_Wraith_Teleport::OnAppearEvent);
 		AppearTask->ReadyForActivation();
 	}
-	
-	UAbilityTask_PlayMontageAndWait* MontageTask = UAbilityTask_PlayMontageAndWait::CreatePlayMontageAndWaitProxy(this, NAME_None,TeleportAnimMontage);
+
+	UAbilityTask_PlayMontageAndWait *MontageTask = UAbilityTask_PlayMontageAndWait::CreatePlayMontageAndWaitProxy(this, NAME_None, TeleportAnimMontage);
 	if (!MontageTask)
 	{
 		RemoveInvulnerableTag();
-		EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo,true,false);
+		EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true, false);
 		return;
 	}
-	
-	MontageTask->OnCompleted.AddDynamic(this,&UGA_Wraith_Teleport::OnMontageEnded);
-	MontageTask->OnBlendOut.AddDynamic(this,&UGA_Wraith_Teleport::OnMontageEnded);
-	MontageTask->OnInterrupted.AddDynamic(this,&UGA_Wraith_Teleport::OnMontageEnded);
-	MontageTask->OnCancelled.AddDynamic(this,&UGA_Wraith_Teleport::OnMontageEnded);
+
+	MontageTask->OnCompleted.AddDynamic(this, &UGA_Wraith_Teleport::OnMontageEnded);
+	MontageTask->OnBlendOut.AddDynamic(this, &UGA_Wraith_Teleport::OnMontageEnded);
+	MontageTask->OnInterrupted.AddDynamic(this, &UGA_Wraith_Teleport::OnMontageEnded);
+	MontageTask->OnCancelled.AddDynamic(this, &UGA_Wraith_Teleport::OnMontageEnded);
 	MontageTask->ReadyForActivation();
 }
 
 void UGA_Wraith_Teleport::OnVanishEvent(FGameplayEventData Payload)
 {
-	AActor* Avatar = GetAvatarActorFromActorInfo();
+	AActor *Avatar = GetAvatarActorFromActorInfo();
 	if (!Avatar)
 	{
 		return;
 	}
-	
+
 	SetTeleportVisualsHidden(true);
-	Avatar->SetActorLocation(CachedDestination , false , nullptr , ETeleportType::TeleportPhysics);
-	
-	if (UAbilitySystemComponent* SourceASC = GetAbilitySystemComponentFromActorInfo())
+	Avatar->SetActorLocation(CachedDestination, false, nullptr, ETeleportType::TeleportPhysics);
+
+	if (UAbilitySystemComponent *SourceASC = GetAbilitySystemComponentFromActorInfo())
 	{
 		FGameplayCueParameters EndCueParams;
 		EndCueParams.Location = CachedDestination;
@@ -133,18 +128,16 @@ void UGA_Wraith_Teleport::OnAppearEvent(FGameplayEventData Payload)
 	SetTeleportVisualsHidden(false);
 }
 
-
 void UGA_Wraith_Teleport::OnMontageEnded()
 {
 	SetTeleportVisualsHidden(false);
 	RemoveInvulnerableTag();
-	EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo,true,false);
+	EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true, false);
 }
-
 
 void UGA_Wraith_Teleport::RemoveInvulnerableTag()
 {
-	if (UAbilitySystemComponent* ASC = GetAbilitySystemComponentFromActorInfo())
+	if (UAbilitySystemComponent *ASC = GetAbilitySystemComponentFromActorInfo())
 	{
 		ASC->RemoveLooseGameplayTag(BlackoutGameplayTags::State_Invulnerable);
 	}
@@ -152,7 +145,7 @@ void UGA_Wraith_Teleport::RemoveInvulnerableTag()
 
 void UGA_Wraith_Teleport::SetTeleportVisualsHidden(bool bHidden)
 {
-	AActor* Avatar = GetAvatarActorFromActorInfo();
+	AActor *Avatar = GetAvatarActorFromActorInfo();
 	if (!Avatar)
 	{
 		return;
@@ -163,9 +156,9 @@ void UGA_Wraith_Teleport::SetTeleportVisualsHidden(bool bHidden)
 		PreviousTeleportHiddenStates.Reset();
 	}
 
-	TArray<UPrimitiveComponent*> PrimitiveComponents;
+	TArray<UPrimitiveComponent *> PrimitiveComponents;
 	Avatar->GetComponents<UPrimitiveComponent>(PrimitiveComponents);
-	for (UPrimitiveComponent* PrimitiveComponent : PrimitiveComponents)
+	for (UPrimitiveComponent *PrimitiveComponent : PrimitiveComponents)
 	{
 		if (!PrimitiveComponent || PrimitiveComponent->IsA<UWidgetComponent>())
 		{
@@ -181,7 +174,7 @@ void UGA_Wraith_Teleport::SetTeleportVisualsHidden(bool bHidden)
 		}
 
 		const TWeakObjectPtr<UPrimitiveComponent> ComponentKey(PrimitiveComponent);
-		if (const bool* PreviousHiddenState = PreviousTeleportHiddenStates.Find(ComponentKey))
+		if (const bool *PreviousHiddenState = PreviousTeleportHiddenStates.Find(ComponentKey))
 		{
 			PrimitiveComponent->SetHiddenInGame(*PreviousHiddenState, false);
 		}
